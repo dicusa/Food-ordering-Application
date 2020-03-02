76d7eeae89d952b486bbf5a3d56d8d142d677ae6
Yash <57257390+dicusa@users.noreply.github.com>
2020-03-02 18:10:15 +0530

    Update YelpApi.js

--- src/api/YelpApi.js
+++ src/api/YelpApi.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer naIFExOFXeBhBiRHTThFsR-Jx67z4-nSZFC2Jqm2ob514yj6_hIa-r5A9OF3DxBu2PuyVJRNGqo6VyyYPvZQcBv6aI4T_FTOhVBYtEjaZiFoM6NJwqxkgH-NYYw2XnYx'
+        Authorization: 'Bearer <your API Key>'
     }
 });
